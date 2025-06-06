01328a82874d11c8362e7e6be4e7ea08609c7518
Andrea PAYSSERAND <paysserand.a@intlocal.univ-amu.fr>
2025-06-06 15:23:07 +0200

    CHange container path

--- segmentation/run_freesurfer.sh
+++ segmentation/run_freesurfer.sh
@@ -18,8 +18,10 @@ echo $FREESURFER_DIR
 mkdir -p $FREESURFER_DIR
 mkdir -p $FREESURFER_DIR/outputs
 
-# Remove existing subject folder
-rm -r $FREESURFER_DIR/outputs/$1
+# Remove existing subject folder only if it exists
+if [ -d "$FREESURFER_DIR/outputs/$1" ]; then
+  rm -r "$FREESURFER_DIR/outputs/$1"
+fi
 
 # Executer le conteneur avec Singularity
-singularity exec -B $SUBJECTS_DIR:/mnt -B $FREESURFER_DIR/outputs:/output -B $FREESURFER_DIR/license:/license --env FS_LICENSE=/license/license.txt /scratch/lhashimoto/freesurfer-7.4.1.sif bash -c "source /usr/local/freesurfer/SetUpFreeSurfer.sh && recon-all -all -s $1 -i /mnt/$1/ses-01/anat/$1_ses-01_T1w.nii.gz -T2 /mnt/$1/ses-01/anat/$1_ses-01_T2w.nii.gz -T2pial -sd /output"
+singularity exec -B $SUBJECTS_DIR:/mnt -B $FREESURFER_DIR/outputs:/output -B $FREESURFER_DIR/license:/license --env FS_LICENSE=/license/license.txt /scratch/lhashimoto/freesurfer-7.4.1.sif bash -c "source /usr/local/freesurfer/SetUpFreeSurfer.sh && recon-all -all -s $1 -i /mnt/sub-$1/ses-01/anat/$1_ses-01_T1w.nii.gz -T2 /mnt/$1/ses-01/anat/$1_ses-01_T2w.nii.gz -T2pial -sd /output"
